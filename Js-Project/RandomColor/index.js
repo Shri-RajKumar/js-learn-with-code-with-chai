7e4d4b8682ad0fa6eb4a2df4b3192fdad5567aee
Raj Kumar <115598973+Shri-RajKumar@users.noreply.github.com>
2023-11-16 10:42:34 +0530

    index.js

---
+++ Js-Project/RandomColor/index.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title> Count timer</title>
+	<link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+
+	<div class="main">
+		<div class="container">
+			<h2 id="color-code">#fff</h2>
+			<button id="btn">Click me</button>
+		</div>
+	</div>
+
+	<script src="./js/app.js"></script>
+
+
+</body>
+
+</html>
